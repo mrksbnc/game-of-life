644233032796f91f170419cab6edf786a385a061
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2026-04-06 17:52:29 +0200

    feat(core): new embed mode support added

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,13 +1,14 @@
-import { cpSync } from 'node:fs';
-import { build } from 'esbuild';
+import { cpSync } from "node:fs";
+import { build } from "esbuild";
 
 await build({
-	bundle: true,
-	minify: true,
-	format: 'esm',
-	outdir: 'dist',
-	entryPoints: ['src/index.ts'],
+  bundle: true,
+  minify: true,
+  format: "esm",
+  outdir: "dist",
+  entryPoints: ["src/index.ts", "src/embed.ts"],
 });
 
-cpSync('index.html', 'dist/index.html');
-cpSync('src/index.css', 'dist/index.css');
+cpSync("index.html", "dist/index.html");
+cpSync("embed.html", "dist/embed.html");
+cpSync("src/index.css", "dist/index.css");
---
+++ src/embed.ts
@@ -0,0 +1,16 @@
+import { GameOfLife } from "./core/game-of-life";
+import { Theme, setTheme } from "./theme/theme";
+
+const params = new URLSearchParams(window.location.search);
+const themeParam = params.get("theme");
+
+const theme = themeParam === Theme.Light || themeParam === Theme.Dark ? themeParam : Theme.Dark;
+
+setTheme(theme);
+
+const game = new GameOfLife({
+  canvasId: "game-of-life__canvas",
+});
+
+game.randomize();
+game.start();
