b5a4f9f87f2c38e9d03cf4d0c52f5dc8f63cbf63
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2026-04-07 12:42:20 +0200

    chore(lint): fmt

--- src/embed.ts
+++ src/embed.ts
@@ -1,15 +1,15 @@
-import { GameOfLife } from "./core/game-of-life";
-import { Theme, setTheme } from "./theme/theme";
+import { GameOfLife } from './core/game-of-life';
+import { Theme, setTheme } from './theme/theme';
 
 const params = new URLSearchParams(window.location.search);
-const themeParam = params.get("theme");
+const themeParam = params.get('theme');
 
 const theme = themeParam === Theme.Light || themeParam === Theme.Dark ? themeParam : Theme.Dark;
 
 setTheme(theme);
 
 const game = new GameOfLife({
-  canvasId: "game-of-life__canvas",
+	canvasId: 'game-of-life__canvas',
 });
 
 game.randomize();
